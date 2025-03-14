d78a170e73b52698db34f8e7d9d4f77cfdad69f3
xLightingBlade <gabrielenigro02@gmail.com>
2025-03-14 15:29:30 +0100

    scheletro di robe bayesiane, piccoli refactor

--- main.py
+++ main.py
@@ -1,40 +1,29 @@
+from collections import Counter
+from time import sleep
+
+import matplotlib.pyplot as plt
+import networkx as nx
 import pandas
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import pgmpy.base
-from skopt import BayesSearchCV
-from sklearn.preprocessing import RobustScaler
-import tensorflow as tf
-import seaborn as sns
-from sklearn.manifold import TSNE
-import matplotlib.lines as mlines
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC, LinearSVC
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.decomposition import PCA
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import IsolationForest
-from sklearn.ensemble import RandomForestClassifier
 from imblearn.over_sampling import SMOTE
-from sklearn.metrics import accuracy_score
-from skopt.space import Real, Categorical, Integer
-import json
-import collections
-from collections import Counter
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.pipeline import make_pipeline
-from sklearn.metrics import precision_score, recall_score, f1_score, roc_auc_score, accuracy_score, \
-    classification_report, make_scorer
-from collections import Counter
-from sklearn.model_selection import KFold, StratifiedKFold, cross_validate
-
-from pgmpy.estimators import MaximumLikelihoodEstimator, HillClimbSearch
+from pandas import DataFrame
+from pgmpy.estimators import MaximumLikelihoodEstimator, HillClimbSearch, ExpectationMaximization
 from pgmpy.inference import VariableElimination
 from pgmpy.models import BayesianNetwork
-import pickle
-from pgmpy.readwrite import BIFReader, BIFWriter
-import networkx as nx
+from pgmpy.readwrite import BIFWriter, BIFReader
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.manifold import TSNE
+from sklearn.metrics import precision_score, recall_score, f1_score, roc_auc_score
+from sklearn.model_selection import cross_validate
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import RobustScaler, KBinsDiscretizer
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from skopt import BayesSearchCV
+
 LABEL_GENUINE = 0
 LABEL_FRAUD = 1
 TEST_SIZE = .2
@@ -55,19 +44,23 @@ def dataset_load_and_explore(path):
     print(genuine_proportion)
     return og_dataset
 
-def dataset_scale(data):
+def dataset_scale(data:DataFrame):
+    x = data.drop('Class', axis=1)
+    y = data['Class']
+    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=42, test_size=TEST_SIZE)
     scaler = RobustScaler()
-    columns_to_scale = data[['Amount', 'Time']]
-    columns_to_scale = columns_to_scale.to_numpy()
-    scaled = scaler.fit_transform(columns_to_scale)
-    scaled_columns = pd.DataFrame(scaled, columns=['Amount', 'Time'])
-    print(scaled_columns)
-    scaled_dataset = data.copy()
-    scaled_dataset['Amount'] = scaled_columns['Amount']
-    scaled_dataset['Time'] = scaled_columns['Time']
-    return scaled_dataset
+    x_train_scaled = scaler.fit_transform(x_train)
+    x_test_scaled = scaler.transform(x_test)
+    x_train_scaled = pd.DataFrame(x_train_scaled, columns=x.columns, index=x_train.index)
+    x_test_scaled = pd.DataFrame(x_test_scaled, columns=x.columns, index=x_test.index)
+    train_data = pd.concat([x_train_scaled, y_train], axis=1)
+    test_data = pd.concat([x_test_scaled, y_test], axis=1)
+    data = pd.concat([train_data, test_data], ignore_index=True)
+    print("Dataframe 'scalato'")
+    print(data.describe())
+    return data
 
-def supervised_base_learning_cv(classifiers, x_train, y_train, over_sampled):
+def supervised_base_cross_validate(classifiers, x_train, y_train, over_sampled):
     if not over_sampled:
         print(f"before SMOTE sampling, counter(y) = {Counter(y_train)}")
         #normale cross validation stratificata (per dataset sbilanciati), con solo scaling di amount e time nel dataset
@@ -152,7 +145,7 @@ def grid_search_hyperparam(classifiers, x_train, y_train, x_test, y_test, over_s
                 i = i + 1
 
 
-def get_bayes_search_list(classifiers):
+def  get_bayes_search_list(classifiers):
     bayes_logreg_param_search = BayesSearchCV(
         classifiers["LogisticRegression"],
         {
@@ -242,8 +235,9 @@ def main():
     knn_grid = {'n_neighbors': [2,5,10,15]}
     dec_tree_grid = {'max_depth': [None,2, 5, 7, 10]}
     grid_search_obj = [logreg_grid, svc_grid, rf_grid, knn_grid, dec_tree_grid]
-    #supervised_base_learning_cv(supervised_base_classifiers, x_train, y_train, False)
-    #supervised_base_learning_cv(supervised_base_classifiers, x_train_oversampled, y_train_oversampled, True)
+    #supervised_base_cross_validate(supervised_base_classifiers, x_train, y_train, False)
+    #supervised_base_cross_validate(supervised_base_classifiers, x_train_oversampled, y_train_oversampled, True)
+    #TODO: visualizzare curve apprendimento e risultati tramite grafico
     #TODO: refactor di sti due tuning, troppe ripetizioni di codice
     #bayes_search_hyperparam(x_train, y_train, x_test, y_test, False, bayes_search_obj)
     #bayes_search_hyperparam(x_train_oversampled, y_train_oversampled, x_test, y_test, True, bayes_search_obj)
@@ -254,13 +248,14 @@ def main():
     #tsne_and_visualize(x_train_oversampled, 100, 5000)
     #tsne_and_visualize(x_train_oversampled, 50, 10000)
     #tsne_and_visualize(x_train, 50, 10000)
-    # tsne_and_visualize(x_train, 80, 10000)
+    #tsne_and_visualize(x_train, 80, 10000)
+
     # https://www.kaggle.com/code/sifodhara/credit-card-fraud-detection-using-isolation-forest per dei plot sui dati, da mettere qua per abbellire sto progetto
-    # Initialize and fit the Isolation Forest model
+    #Isolation forest per visualizzare anomalie
     """
     contamination_ratio = scaled_dataset["Class"].sum() / len(scaled_dataset)
     print("Contamination ratio:", contamination_ratio)
-
+    
     iso = IsolationForest(contamination=contamination_ratio, n_estimators=200, random_state=42, n_jobs=-1, verbose=2)
     iso.fit(x)
 
@@ -296,26 +291,119 @@ def main():
     plt.savefig("isolation forest.png")
     plt.show()
     """
-    bayesian_network_structure_learning(scaled_dataset, 10000, True)
+    #testo le varie combinazioni di discretizzazione su modelli base, per capire il giusto numero di bins
+    #data_discretization_test(scaled_dataset, supervised_base_classifiers)
+    #results_df = pd.read_csv("discretization results.csv")
+    #print(results_df.sort_values('f1', ascending=False))
+    #dunque controllo i risultati, sceglo quei parametri per la discretizzazione del dataframe
+    #discretizzato, posso poi fare structure learning, ci provo due volte per vedere se più sample rivelano strutture diverse
+    #per ora uso 20 bins per fargli (provare) fare qualcosa overnight
+    #bayesian_network_structure_learning(scaled_dataset, 1000, 20, "kmeans")
+    bayesian_network_simulate_single_sample('bnet 100000 samples 20 bins kmeans strategy 6 features max likelihood.bif')
 
-def bayesian_network_structure_learning(dataframe: pandas.DataFrame, n_samples, is_data_continuous):
-    df = get_dataframe_sample(dataframe, n_samples)
+def data_discretization_test(data, classifiers):
+    x = data.drop('Class', axis=1)
+    y = data['Class']
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=TEST_SIZE, random_state=42)
+
+    bins_list = [5, 10, 15, 20, 30, 50]
+    strategies = ["uniform", "quantile", "kmeans"]
+
+    results = []
+
+    for strategy in strategies:
+        for bins in bins_list:
+            for classifier in classifiers.values():
+                kbins = KBinsDiscretizer(n_bins=bins, encode="ordinal", strategy=strategy)
+                x_train_discrete = kbins.fit_transform(x_train)
+                x_test_discrete = kbins.transform(x_test)
+
+                classifier.fit(x_train_discrete, y_train)
+
+                y_pred = classifier.predict(x_test_discrete)
+                f1 = f1_score(y_test, y_pred)
+                precision = precision_score(y_test, y_pred)
+                recall = recall_score(y_test, y_pred)
+
+                results.append({
+                    "classifier": classifier.__class__.__name__,
+                    "strategy": strategy,
+                    "n_bins": bins,
+                    "f1": f1,
+                    "precision": precision,
+                    "recall": recall,
+                })
+                print(
+                    f"Classifier: {classifier.__class__.__name__}, Strategy: {strategy}, n_bins: {bins} | F1: {f1:.4f},"
+                    f" Precision: {precision:.4f}, Recall: {recall:.4f}\n")
+
+    results_df = pd.DataFrame(results)
+    print("\nResults:")
+    print(results_df)
+    results_df.to_csv("discretization results.csv", index=False)
+
+
+def discretize_df(dataframe:DataFrame, bins, strategy):
+    print('prima')
+    print(dataframe.head())
+    kbins = KBinsDiscretizer(n_bins=bins, encode="ordinal", strategy=strategy)
+    cols = [col for col in dataframe.columns if col != 'Class']
+    x_discrete = kbins.fit_transform(dataframe[cols])
+    discrete_df = pd.DataFrame(x_discrete, columns=cols, index=dataframe.index)
+    discrete_df['Class'] = dataframe['Class']
+    discrete_df = discrete_df[dataframe.columns]
+    print(discrete_df.head())
+    return discrete_df
+
+
+#tipizzo il parametro dataframe per aiutarmi con l`IDE
+def bayesian_network_structure_learning(dataframe: pandas.DataFrame, n_samples, discrete_bins, discrete_strategy):
+    df = dataframe_choose_cols_and_sample(dataframe, n_samples, discrete_bins, discrete_strategy)
     net_estimator = HillClimbSearch(df)
-    bayesian_network:pgmpy.base.DAG = net_estimator.estimate()
-    nx.draw(bayesian_network)
-    //capire se funge il plt savefig e se innanzitutto funge networkx, altrimenti provare con daft
-    plt.savefig(f"bayesian network {n_samples} samples {'continuous data' if is_data_continuous else ''}.png")
-    plt.draw()
+    bayesian_network: pgmpy.base.DAG = net_estimator.estimate()
     bayesian_network_model = BayesianNetwork(bayesian_network.edges())
     bayesian_network_model.fit(df, estimator=MaximumLikelihoodEstimator, n_jobs=-1)
-    //https://pgmpy.org/readwrite/bif.html
+    #https://pgmpy.org/readwrite/bif.html
     writer = BIFWriter(bayesian_network_model)
-    writer.write_bif(filename=f'bnet_{n_samples}samples_{'continuous' if is_data_continuous else ''}.bif')
-    
-def get_dataframe_sample(dataframe, n_samples):
+    writer.write_bif(filename=f'bnet {n_samples} samples {discrete_bins} bins {discrete_strategy} strategy {len(df.columns)} features max likelihood.bif')
+    #TODO: modificare un pò la visualizzazione della rete, troppo obvious cosi
+    G = nx.MultiDiGraph(bayesian_network_model.edges())
+    pos = nx.spring_layout(G, iterations=100, k=2,
+                           threshold=5, pos=nx.spiral_layout(G))
+    nx.draw_networkx_nodes(G, pos, node_size=150, node_color="#ff574c")
+    nx.draw_networkx_labels(
+        G,
+        pos,
+        font_size=10,
+        font_weight="bold",
+        clip_on=True,
+        horizontalalignment="center",
+        verticalalignment="bottom",
+    )
+    nx.draw_networkx_edges(
+        G,
+        pos,
+        arrows=True,
+        arrowsize=7,
+        arrowstyle="->",
+        edge_color="purple",
+        connectionstyle="angle3,angleA=90,angleB=0",
+        min_source_margin=1,
+        min_target_margin=1,
+        edge_vmin=2,
+        edge_vmax=2,
+    )
+
+    plt.title("BAYESIAN NETWORK GRAPH")
+    plt.savefig(f"bayesian network {n_samples} samples {discrete_bins} bins {discrete_strategy} strategy {len(df.columns)} features.png")
+    plt.show()
+    plt.clf()
+
+def dataframe_choose_cols_and_sample(dataframe:DataFrame, n_samples, bins, strategy):
     #prima uno shuffle del dataframe
     dataframe = dataframe.sample(frac=1).reset_index(drop=True)
-    x = dataframe.drop('Class', axis=1)
+    x = dataframe[['Time', 'V1', 'V2', 'V3', 'Amount']]
     y = dataframe['Class']
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=TEST_SIZE, random_state=42)
     sm = SMOTE(random_state=42, sampling_strategy=.5)
@@ -326,11 +414,33 @@ def get_dataframe_sample(dataframe, n_samples):
     fraud_df = x_train.loc[x_train['Class'] == 1 ].sample(n_samples // 2)
     real_df = x_train.loc[x_train['Class'] == 0].sample(n_samples // 2)
     df_sample = pd.concat([fraud_df, real_df], ignore_index=True)
-    return df_sample
+    discrete_sample = discretize_df(df_sample, bins, strategy)
+    print("discrete sample df")
+    print(discrete_sample.head())
+    return discrete_sample
 
+def bayesian_network_inference(model_path, data_to_predict:pd.Series):
+    model = get_bayesian_network_model(model_path)
+    inference = VariableElimination(model)
+    data_to_predict = {
+        "Amount" : data_to_predict['Amount'],
+        "V1": data_to_predict['V1'],
+        "V2": data_to_predict['V2'],
+        "V3": data_to_predict['V3'],
+        "Time": data_to_predict['Time']
+    }
+    res = inference.query(variables = ['Class'], evidence =data_to_predict)
+    print(res)
 
+def bayesian_network_simulate_single_sample(model_path):
+    model = get_bayesian_network_model(model_path)
+    sample = model.simulate(n_samples=1)
+    print(sample)
 
-
+def get_bayesian_network_model(model_path):
+    reader = BIFReader(model_path)
+    model = reader.get_model()
+    return model
 
 if __name__ == "__main__":
     main()
